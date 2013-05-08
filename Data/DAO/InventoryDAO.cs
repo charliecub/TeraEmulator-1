a628f145de58b4da65bea279c6930b9f359abb90
jenose <jenose_x@hotmail.com>
2013-05-08 12:29:42 +0700

    Forgot commit files

---
+++ Data/DAO/InventoryDAO.cs
@@ -0,0 +1,143 @@
+﻿using Data.Enums.Item;
+using Data.Structures.Account;
+using Data.Structures.Player;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Data.DAO
+{
+    public class InventoryDAO : BaseDAO
+    {
+        private MySqlConnection InvenDAOConnection;
+
+        public InventoryDAO(string conStr)
+            : base(conStr)
+        {
+            InvenDAOConnection = new MySqlConnection(conStr);
+            InvenDAOConnection.Open();
+            Log.Info("DAO: InventoryDAO Initialized.");
+        }
+
+        public bool AddItem(Player player, StorageType type, KeyValuePair<int, StorageItem> kvp)
+        {
+            string cmdString = "SELECT * FROM inventory WHERE PlayerId=?pid AND ItemId=?itemid AND Slot=?slot";
+            MySqlCommand command = new MySqlCommand(cmdString, InvenDAOConnection);
+            command.Parameters.AddWithValue("?pid", player.Id);
+            command.Parameters.AddWithValue("?itemid", kvp.Value.ItemId);
+            command.Parameters.AddWithValue("?slot", kvp.Key);
+            MySqlDataReader reader = command.ExecuteReader();
+            bool isExists = reader.HasRows;
+            reader.Close();
+
+            if (!isExists)
+            {
+                cmdString = "INSERT INTO inventory "
+                + "(`AccountName`,`PlayerId`,`ItemId`,`Amount`,`Color`,`Slot`,`StorageType`) "
+                + "VALUES (?aname, ?pid, ?itemid, ?count, ?color, ?slot, ?type); SELECT LAST_INSERT_ID();";
+                command = new MySqlCommand(cmdString, InvenDAOConnection);
+                command.Parameters.AddWithValue("?aname", player.AccountName);
+                command.Parameters.AddWithValue("?pid", player.Id);
+                command.Parameters.AddWithValue("?itemid", kvp.Value.ItemId);
+                command.Parameters.AddWithValue("?count", kvp.Value.Count);
+                command.Parameters.AddWithValue("?color", kvp.Value.Color);
+                command.Parameters.AddWithValue("?slot", kvp.Key);
+                command.Parameters.AddWithValue("?type", type.ToString());
+            }
+            else
+            {
+                cmdString = "UPDATE inventory SET "
+                + "`Amount`=?count,`Color`=?color,`Slot`=?slot,`StorageType`=?type WHERE `ItemId`=?itemid AND `PlayerId`=?pid";
+                command = new MySqlCommand(cmdString, InvenDAOConnection);
+                command.Parameters.AddWithValue("?count", kvp.Value.Count);
+                command.Parameters.AddWithValue("?color", kvp.Value.Color);
+                command.Parameters.AddWithValue("?slot", kvp.Key);
+                command.Parameters.AddWithValue("?type", type.ToString());
+                command.Parameters.AddWithValue("?itemid", kvp.Value.ItemId);
+                command.Parameters.AddWithValue("?pid", player.Id);
+            }
+
+            try
+            {
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Log.ErrorException("AddItem Error", ex);
+            }
+
+            return false;
+        }
+
+        public bool SaveStorage(Player player, Storage storage)
+        {
+            if (storage.Items.Count > 0)
+            {
+                foreach (var item in storage.Items)
+                    AddItem(player, storage.StorageType, item);
+
+                return true;
+            }
+            return false;
+        }
+
+        public Dictionary<int, StorageItem> LoadStorage(Player player, StorageType type)
+        {
+            string cmdString = "SELECT * FROM inventory WHERE PlayerId=?id AND StorageType=?type";
+            MySqlCommand command = new MySqlCommand(cmdString, InvenDAOConnection);
+            command.Parameters.AddWithValue("?id", player.Id);
+            command.Parameters.AddWithValue("?type", type.ToString());
+            MySqlDataReader reader = command.ExecuteReader();
+
+            Dictionary<int, StorageItem> items = new Dictionary<int, StorageItem>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    StorageItem item = new StorageItem()
+                    {
+                        ItemId = reader.GetInt32(3),
+                        Count = reader.GetInt32(4),
+                        Color = reader.GetInt32(5),
+                    };
+                    items.Add(reader.GetInt32(6), item);
+                }
+            }
+            reader.Close();
+
+            return items;
+        }
+
+        public Dictionary<int, StorageItem> LoadAccountStorage(Account account)
+        {
+            string cmdString = "SELECT * FROM inventory WHERE AccountName=?aname AND StorageType=?type";
+            MySqlCommand command = new MySqlCommand(cmdString, InvenDAOConnection);
+            command.Parameters.AddWithValue("?aname", account.Name);
+            command.Parameters.AddWithValue("?type", StorageType.AccountWarehouse.ToString());
+            MySqlDataReader reader = command.ExecuteReader();
+
+            Dictionary<int, StorageItem> items = new Dictionary<int, StorageItem>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    StorageItem item = new StorageItem()
+                    {
+                        ItemId = reader.GetInt32(3),
+                        Count = reader.GetInt32(4),
+                        Color = reader.GetInt32(5),
+                    };
+                    items.Add(reader.GetInt32(6), item);
+                }
+            }
+            reader.Close();
+
+            return items;
+        }
+    }
+}
---
+++ Data/DAO/QuestDAO.cs
@@ -0,0 +1,124 @@
+﻿using Data.Enums;
+using Data.Structures.Player;
+using Data.Structures.Quest;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Data.DAO
+{
+    public class QuestDAO : BaseDAO
+    {
+        private MySqlConnection QuestDAOConnection;
+
+        public QuestDAO(string conStr)
+            : base(conStr)
+        {
+            QuestDAOConnection = new MySqlConnection(conStr);
+            QuestDAOConnection.Open();
+            Log.Info("DAO: QuestDAO Initialized.");
+        }
+
+        public void AddQuest(Player player, QuestData questdata)
+        {
+            string cmdString = "SELECT * FROM questdata WHERE QuestId=?qid AND PlayerId=?pid";
+            MySqlCommand command = new MySqlCommand(cmdString, QuestDAOConnection);
+            command.Parameters.AddWithValue("?qid", questdata.QuestId);
+            command.Parameters.AddWithValue("?pid", player.Id);
+            MySqlDataReader reader = command.ExecuteReader();
+            bool isExists = reader.HasRows;
+            reader.Close();
+
+            if (!isExists)
+            {
+                cmdString = "INSERT INTO questdata "
+                + "(`PlayerId`,`QuestId`,`Status`,`Step`,`Counters`) "
+                + "VALUES (?pid, ?qid, ?qstatus, ?qstep, ?qcounter);";
+                command = new MySqlCommand(cmdString, QuestDAOConnection);
+                command.Parameters.AddWithValue("?pid", player.Id);
+                command.Parameters.AddWithValue("?qid", questdata.QuestId);
+                command.Parameters.AddWithValue("?qstatus", questdata.Status.ToString());
+                command.Parameters.AddWithValue("?qstep", questdata.Step);
+                command.Parameters.AddWithValue("?qcounter", string.Join(",", questdata.Counters));
+            }
+            else
+            {
+                cmdString = "UPDATE questdata SET"
+                + "`Status`=?qstatus,`Step`=?qstep,`Counters`=?qcounter WHERE QuestId=?qid AND PlayerId=?pid";
+                command = new MySqlCommand(cmdString, QuestDAOConnection);
+                command.Parameters.AddWithValue("?qstatus", questdata.Status.ToString());
+                command.Parameters.AddWithValue("?qstep", questdata.Step);
+                command.Parameters.AddWithValue("?qcounter", string.Join(",", questdata.Counters));
+                command.Parameters.AddWithValue("?qid", questdata.QuestId);
+                command.Parameters.AddWithValue("?pid", player.Id);
+            }
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Log.ErrorException("SaveQuest Error", ex);
+            }
+        }
+
+        public QuestData LoadQuest(Player player, int questId)
+        {
+            string cmdString = "SELECT * FROM questdata WHERE QuestId=?qid AND PlayerId=?pid";
+            MySqlCommand command = new MySqlCommand(cmdString, QuestDAOConnection);
+            command.Parameters.AddWithValue("?qid", questId);
+            command.Parameters.AddWithValue("?pid", player.Id);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            QuestData quest = new QuestData(questId);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    quest = new QuestData(questId)
+                    {
+                        QuestId = reader.GetInt32(1),
+                        Status = (QuestStatus)Enum.Parse(typeof(QuestStatus), reader.GetString(2)),
+                        Step = reader.GetInt32(3),
+                        Counters = reader.GetString(4).Split(',').Select(n => int.Parse(n)).ToList()
+                    };
+                }
+            }
+            reader.Close();
+
+            return quest;
+        }
+
+        public Dictionary<int, QuestData> LoadQuests(Player player)
+        {
+            string cmdString = "SELECT * FROM questdata WHERE PlayerId=?pid";
+            MySqlCommand command = new MySqlCommand(cmdString, QuestDAOConnection);
+            command.Parameters.AddWithValue("?pid", player.Id);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            Dictionary<int, QuestData> questlist = new Dictionary<int, QuestData>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    QuestData quest = new QuestData(0)
+                    {
+                        QuestId = reader.GetInt32(2),
+                        Status = (QuestStatus)Enum.Parse(typeof(QuestStatus), reader.GetString(3)),
+                        Step = reader.GetInt32(4),
+                        Counters = reader.GetString(5).Split(',').Select(n => int.Parse(n)).ToList()
+                    };
+                    questlist.Add(quest.QuestId, quest);
+                }
+            }
+            reader.Close();
+
+            return questlist;
+        }
+    }
+}
